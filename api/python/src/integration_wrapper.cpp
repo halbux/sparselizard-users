da65c3ef43751c7b4839bc9f70df0d579a37021e
Alexandre Halbach <alexandre.halbach@gmail.com>
2021-11-01 22:45:17 +0200

    empty integration_wrapper

--- api/python/src/integration_wrapper.cpp
+++ api/python/src/integration_wrapper.cpp
@@ -5,12 +5,6 @@ namespace py = pybind11;
 void init_integration(py::module &m)
 {
     py::class_<integration>(m, "integration")
-    
-        .def(py::init<>())
-        .def(py::init<int, expression, int, int>(), py::arg("physreg"), py::arg("tointegrate"), py::arg("integrationorderdelta")=0, py::arg("blocknumber")=0)
-        .def(py::init<int, expression, expression, int, int>(), py::arg("physreg"), py::arg("meshdeform"), py::arg("tointegrate"), py::arg("integrationorderdelta")=0, py::arg("blocknumber")=0)
-        .def(py::init<int, int, expression, int, int>(), py::arg("physreg"), py::arg("numcoefharms"), py::arg("tointegrate"), py::arg("integrationorderdelta")=0, py::arg("blocknumber")=0)
-        .def(py::init<int, int, expression, expression, int, int>(), py::arg("physreg"), py::arg("numcoefharms"), py::arg("meshdeform"), py::arg("tointegrate"), py::arg("integrationorderdelta")=0, py::arg("blocknumber")=0)
         
     ;
 }
